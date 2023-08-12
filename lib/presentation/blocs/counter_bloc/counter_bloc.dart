5aeddb725b9cce53f31c358cd23c9201ef5bd0f0
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-08-12 14:22:19 -0500

    [imp] :: Consume Methods FlutterBloc

--- lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -5,13 +5,31 @@ part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(const CounterState()) {
+  CounterBloc() : super(const CounterState(counter: 44)) {
     on<CounterIncreased>(_onCounterIncreased);
+
+    on<CounterReset>(_onCounterReset);
   }
+
+  //? Logic Methods
 
   void _onCounterIncreased(CounterIncreased event, Emitter<CounterState> emit) {
     emit(state.copyWith(
         counter: state.counter + event.value,
         transactionCount: state.transactionCount + 1));
+  }
+
+  void _onCounterReset(CounterReset event, Emitter<CounterState> emit) {
+    emit(state.copyWith(counter: 0));
+  }
+
+  //? Public Methods
+
+  void increaseCounterByX([int value = 1]) {
+    add(CounterIncreased(value));
+  }
+
+  void resetCounter() {
+    add(const CounterReset());
   }
 }
--- lib/presentation/screens/block_counter_screen.dart
+++ lib/presentation/screens/block_counter_screen.dart
@@ -1,33 +1,71 @@
+import 'package:fbloc_cubits_forms/presentation/blocs/counter_bloc/counter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BlocCounterScreen extends StatelessWidget {
   const BlocCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterBloc(),
+      child: const BlocCounterView(),
+    );
+  }
+}
+
+class BlocCounterView extends StatelessWidget {
+  const BlocCounterView({
+    super.key,
+  });
+
+  void increaseCounterBy(BuildContext context, [int value = 1]) {
+    //! First way to do it
+    // context.read<CounterBloc>().add(CounterIncreased(value));
+    //? Second way to do it
+    context.read<CounterBloc>().increaseCounterByX(value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BLoC Counter Screen'),
+        title: context.select((CounterBloc fbloc) =>
+            Text('Counter: ${fbloc.state.transactionCount}')),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh))
+          IconButton(
+              //! First way to do it
+              // onPressed: () => context.read<CounterBloc>().add(const CounterReset()),
+              //? Second way to do it
+              onPressed: () => context.read<CounterBloc>().resetCounter(),
+              icon: const Icon(Icons.refresh))
         ],
       ),
-      body: const Center(
-        child: Text('BLoC Counter Screen'),
+      body: Center(
+        child: context.select((CounterBloc counterBloc) => Text(
+              '${counterBloc.state.counter}',
+              style: const TextStyle(fontSize: 24),
+            )),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
             heroTag: '1',
-            onPressed: () {},
-            child: const Icon(Icons.add),
+            onPressed: () => increaseCounterBy(context),
+            child: const Text('+1'),
           ),
           const SizedBox(height: 8),
           FloatingActionButton(
             heroTag: '2',
-            onPressed: () {},
-            child: const Icon(Icons.remove),
+            onPressed: () => increaseCounterBy(context, 2),
+            child: const Text('+2'),
+          ),
+          const SizedBox(height: 8),
+          FloatingActionButton(
+            heroTag: '3',
+            onPressed: () => increaseCounterBy(context, 3),
+            child: const Text('+3'),
           ),
         ],
       ),
